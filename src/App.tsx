79831f880685680d9a43d25787f1d95503ca4342
Havrilenko Olexiy <hristinson@gmail.com>
2022-09-22 19:51:06 +0300

    added axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,62 @@
 import React from 'react';
-import LeftBar from './components/LeftBar';
-import Main from './components/Main';
-import RightBar from './components/RightBar';
-
-
 import './App.css';
+import { Button } from 'react-bootstrap';
+import axios from 'axios';
+
+  // let offSet = 0;
+  // const url: string = `https://pokeapi.co/api/v2/pokemon/?offset=${offSet}&limit=20`;
+  // async function (){}
+
+  type User = {
+    id: number;
+    email: string;
+    first_name: string;
+  };
+  
+  type GetUsersResponse = {
+    data: User[];
+  };
+  
+  async function getUsers() {
+    try {
+      // 👇️ const data: GetUsersResponse
+      const { data, status } = await axios.get<GetUsersResponse>(
+        'https://reqres.in/api/users',
+        {
+          headers: {
+            Accept: 'application/json',
+          },
+        },
+      );
+  
+      console.log(JSON.stringify(data, null, 4));
+  
+      // 👇️ "response status is: 200"
+      console.log('response status is: ', status);
+  
+      return data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.log('error message: ', error.message);
+        return error.message;
+      } else {
+        console.log('unexpected error: ', error);
+        return 'An unexpected error occurred';
+      }
+    }
+  }
+  
+
+
+
+
 
 function App() {
+  
+
   return (
     <div className="App">
-      <div>
-      <LeftBar />
-      </div>
-
-      <div>
-      <Main />
-      </div>
-
-
-
-      <div>
-      <RightBar />
-      </div>
-
-
-
+      <Button onClick={()=>{getUsers()}}>Click ME</Button>
     </div>
   );
 }
