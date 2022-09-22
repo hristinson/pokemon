5631812dd984e74ede66d9b4676b4f6c4aeb353a
Havrilenko Olexiy <hristinson@gmail.com>
2022-09-22 20:41:48 +0300

    added useState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Button } from 'react-bootstrap';
 import axios from 'axios';
 
-  type User = {
+
+function App() {
+
+  //const [main: JSON, setMain ] = useState(null)
+  const [off, setOff ] = useState(0)
+  
+
+  type Pokemon = {
     id: number;
     email: string;
     first_name: string;
   };
   
   type GetUsersResponse = {
-    data: User[];
+    data: Pokemon[];
   };
   
-  async function getUsers() {
-    const off = 0;
+  async function getUsers(off: number) {
+   
     const url: string = `https://pokeapi.co/api/v2/pokemon/?offset=${off}&limit=20`;
     try {
 
@@ -22,23 +29,32 @@ import axios from 'axios';
       console.log(data);
       console.log(status);
 
-      return data;
+      //return data;
     } catch (error) {
       if (axios.isAxiosError(error)) {
-        console.log('error message: ', error.message);
-        return error.message;
+        //console.log('error message: ', error.message);
+        //return error.message;
       } else {
         console.log('unexpected error: ', error);
-        return 'An unexpected error occurred';
+        //return 'An unexpected error occurred';
       }
     }
   }
 
-function App() {
+
+
+
+
 
   return (
     <div className="App">
-      <Button onClick={()=>{getUsers()}}>Click ME</Button>
+      <div>
+      {off}
+      </div>
+      
+
+      <Button onClick={()=>{getUsers(0)}}>Click ME</Button>
+      
     </div>
   );
 }
