b7cf7dc34e506331c104072e4873d7a3459aec47
Havrilenko Olexiy <hristinson@gmail.com>
2022-09-22 22:32:23 +0300

    test1

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import './App.css';
 import { Button } from 'react-bootstrap';
 import axios from 'axios';
+import { any } from 'prop-types';
 
 
 function App() {
 
-  //const [main: JSON, setMain ] = useState(null)
+  const [main, setMaint ] = useState({})
   const [off, setOff ] = useState(0)
   
 
@@ -25,18 +26,20 @@ function App() {
     const url: string = `https://pokeapi.co/api/v2/pokemon/?offset=${off}&limit=20`;
     try {
 
-      const { data, status } = await axios.get<GetUsersResponse>(url, {headers: {Accept: 'application/json', },}, );
-      console.log(data);
-      console.log(status);
+      //const { data, status } = 
+      await axios.get<GetUsersResponse>(url, {headers: {Accept: 'application/json', },}, ).then((res)=>{
+        console.log(res.data);
+        setMaint(main => {return 0})
+      });
+     
 
-      //return data;
+      
     } catch (error) {
       if (axios.isAxiosError(error)) {
-        //console.log('error message: ', error.message);
-        //return error.message;
+        
       } else {
         console.log('unexpected error: ', error);
-        //return 'An unexpected error occurred';
+        
       }
     }
   }
@@ -49,7 +52,7 @@ function App() {
   return (
     <div className="App">
       <div>
-      {off}
+      {  }
       </div>
       
 
@@ -57,6 +60,9 @@ function App() {
       
     </div>
   );
+  
+  console.log(main)
+
 }
 
 export default App;
