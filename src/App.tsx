70dcc2a050bf5be3010dabd2db4e402847784d8b
Havrilenko Olexiy <hristinson@gmail.com>
2022-09-26 16:27:53 +0300

    fix bug 26092022/01

--- src/App.tsx
+++ src/App.tsx
@@ -6,40 +6,29 @@ import { PokemonsIF } from './models';
 import  pokemons  from './component/getPokemons';
 import  { pokemonsData }  from './data/pokemons';
 
-// interface ProductProps {
-//   product: PokemonsIF
-// }
-
 function App() {
-
-  // const test: PokemonsIF = {
-  //   name: `Samael`,
-  //   url: `http://github.com`
-  // }
 
   const [pokOpen, setPokOpen] = useState(pokemonsData)
   const [counter, setCounter] = useState(0)
 
   useEffect(() => {
-    //console.log(pokOpen[0])
-     pokemons().then((res)=>{ setPokOpen(res)})
-  
+     pokemons().then((res: PokemonsIF[])=>{ setPokOpen(res)})
   }, []);
   
-
   
   return (
     <div className="App">
      {counter}
-     <Pokemon dataPoki={pokOpen[0]} ></Pokemon>
-     <Pokemon dataPoki={pokOpen[1]} ></Pokemon>
-     <Pokemon dataPoki={pokOpen[3]} ></Pokemon>
-     <Pokemon dataPoki={pokOpen[4]} ></Pokemon>
-     <Pokemon dataPoki={pokOpen[5]} ></Pokemon>
-     <Pokemon dataPoki={pokOpen[6]} ></Pokemon>
+     <Pokemon pokemons={pokOpen[0]} ></Pokemon>
+     <Pokemon pokemons={pokOpen[1]} ></Pokemon>
+     <Pokemon pokemons={pokOpen[2]} ></Pokemon>
+     <Pokemon pokemons={pokOpen[3]} ></Pokemon>
+     <Pokemon pokemons={pokOpen[4]} ></Pokemon>
+     <Pokemon pokemons={pokOpen[5]} ></Pokemon>
+     <Pokemon pokemons={pokOpen[6]} ></Pokemon>
      
      <Button onClick={(a)=>{ setCounter(counter+1); 
-     
+    
       }}>Click me</Button>
     </div>
   );
--- src/component/Pokemon.tsx
+++ src/component/Pokemon.tsx
@@ -5,24 +5,13 @@ interface PokemonProps {
   pokemons: PokemonsIF
 }
 
-function App(props: any) {
+function App(props: PokemonProps) {
 
-  //const [propsState, setPropsState] = useState(0)
-
- 
-  // useEffect(() => {
-  //   //console.log(props)
-  //   setPropsState(p => propsState+1)
-  // },[]);
-
-  //console.log(props.dataPoki.name);
-  
 
   return (
     <div className="border py-5 px-5 rounded flex fle-col items-center mb2">
-        Pokemon {props.dataPoki.name} is ready to fight!
-        
-            </div>
+        Pokemon {props.pokemons.name} is ready to fight!
+    </div>
   );
   
   
